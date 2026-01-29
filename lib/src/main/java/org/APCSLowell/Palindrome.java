6d85e767166259f2cf942c69e705d3bb5032843c
Riley-Lin123 <rtlin94122@gmail.com>
2026-01-29 10:00:15 -0800

    Update Palindrome.java

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,27 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        String s1 = "";
+        String n = "";
+        for (int i = 0; i < s.length(); i++){
+            if (s.substring(i,i+1).equals(" ")){
+                continue;
+            }else{
+                n = n + s.substring(i,i+1);
+                s1 = s1 + s.substring(i,i+1);
+            }
+        }
+        
+        if (s1.toLowerCase().equals(reverse(n.toLowerCase()))){
+            return true;
+        }else return false;
     }
     public String reverse(String s) {
-        String n = new String();
-        // your code here
-        return n;
+        String backward = "";
+       for (int i = s.length(); i > 0; i--){
+        backward = backward + s.substring(i-1,i);
+       }
+       return n;
+        
     }
 }
